fa7a12394287dd7746dd06bc5619e066a56181ba
Muhamed-M <muhamedmuhamedagic03@gmail.com>
2024-09-08 21:51:03 +0200

    update: start script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,12 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ['@nuxt/ui', "@pinia/nuxt"],
+  modules: ['@nuxt/ui', '@pinia/nuxt'],
   colorMode: {
     preference: 'light',
+  },
+  runtimeConfig: {
+    apiUrl: process.env.API_URL || 'http://localhost:4000/api',
+    socketServerUrl: process.env.SOCKET_SERVER_URL || 'ws://localhost:8080',
   },
 });
