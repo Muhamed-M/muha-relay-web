84efda89f646682708d67566bb4220945ae35df9
Muhamed-M <muhamedmuhamedagic03@gmail.com>
2025-12-07 16:23:03 +0100

    added webmanifest for fullscreen support and add to home screen feature

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,17 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Muha Relay',
-      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'manifest', href: '/manifest.webmanifest' },
+        { rel: 'apple-touch-icon', href: '/favicon.svg' },
+      ],
+      meta: [
+        { name: 'theme-color', content: '#000000' },
+        { name: 'mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+        { name: 'apple-mobile-web-app-title', content: 'CoolApp' },
+      ],
     },
   },
   compatibilityDate: '2024-04-03',
